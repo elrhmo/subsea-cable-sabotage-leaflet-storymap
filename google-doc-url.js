cfebf7e6709691a48fda3363107dfe72d61bd340
Elliot Moses <elliot.moses@globalmaritime.com>
2025-01-15 16:21:38 +0000

    populating with content

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = null;
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = null;
